5fc0dc27555c971eb32452086a9acfbf5fc6a190
Chris Anderson <jchris@gmail.com>
2023-11-05 08:42:31 -0800

    connected

--- party/index.ts
+++ party/index.ts
@@ -13,7 +13,7 @@ export default class Server implements Party.Server {
     );
 
     // let's send a message to the connection
-    conn.send("hello from server");
+    conn.send(JSON.stringify({msg:"hello from server"}));
   }
 
   onMessage(message: string, sender: Party.Connection) {
--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -4,14 +4,34 @@ import { Link, useParams } from 'react-router-dom'
 import { useFireproof } from 'use-fireproof'
 import { InlineEditor } from '../components/InlineEditor'
 
+import usePartySocket from 'partysocket/react'
+
+const dbName = localStorage.getItem('dbName') || Math.random().toString(36).substring(2)
+localStorage.setItem('dbName', dbName)
+
+const PUBLIC_PARTYKIT_HOST=`127.0.0.1:1999`
+
 export function Chat() {
   const { id } = useParams<{ id: string }>()
-  const { database } = useFireproof('pout')
+  const { database } = useFireproof(dbName)
+
+  const socket = usePartySocket({
+    host : PUBLIC_PARTYKIT_HOST,
+    room : dbName,
+    onOpen(e) {
+      console.log('open', e)
+    },
+    onMessage(event: MessageEvent<string>) {
+      const message = JSON.parse(event.data)
+      console.log('message', message)
+    }
+  })
+
+  console.log('Chat', socket, dbName, id)
 
   return (
     <div className="w-screen h-screen">
       <div className="flex flex-col flex-grow p-4 overflow-auto">
-
         <div className="flex w-full mt-2 space-x-3 max-w-sm">
           <div className="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div>
           <div>
