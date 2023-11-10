7ca80880ea853e9ed664a255c07f376ea1e0b842
Chris Anderson <jchris@gmail.com>
2023-11-09 16:09:58 -0800

    style

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,12 +4,10 @@ import { ChatBubble, ImageBubble } from '../components/ChatBubbles'
 export function Home() {
   return (
     <div className="">
-
-
       <div className="bg-gray-300 p-4">
         <h1>Cat Chat</h1>
       </div>
-      
+
       <div className="p-4">
         <h2>
           <a href="/profile" className="bg-blue-500 hover:bg-blue-300 font-bold py-2 px-4 rounded">
@@ -18,10 +16,8 @@ export function Home() {
         </h2>
 
         <ImageBubble imgSrc={catImage} alt="Welcome photo" />
-        <ChatBubble
-          imgSrc={catImage}
-          message="Hi, I'm Fluffy, welcome to cat chat. You can ask 'meow' anything. What do you want to know?"
-        />
+        <h2>Ask the cats anything!</h2>
+        <ImageBubble imgSrc={cat2Image} alt="Welcome photo" />
       </div>
     </div>
   )
